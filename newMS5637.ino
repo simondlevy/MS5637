d0cf1e05450bbe35d2941b9179d759daa636f9c8
simondlevy <simon.d.levy@gmail.com>
2018-09-14 22:02:11 -0400

    =Cleanup

--- newMS5637.ino
+++ newMS5637.ino
@@ -30,6 +30,83 @@ int16_t tempCount;            // temperature raw count output
 float   temperature;          // Stores the MPU9250 gyro internal chip temperature in degrees Celsius
 double Temperature, Pressure; // stores MS5637 pressures sensor pressure and temperature
 
+// For the MS5637, we write commands, and the MS5637 sends data in response, rather than directly reading
+// MS5637 registers
+
+static void MS5637Reset()
+{
+    Wire.beginTransmission(MS5637_ADDRESS);  // Initialize the Tx buffer
+    Wire.write(MS5637_RESET);                // Put reset command in Tx buffer
+    Wire.endTransmission();                  // Send the Tx buffer
+}
+
+static void MS5637PromRead(uint16_t * destination)
+{
+    uint8_t data[2] = {0,0};
+    for (uint8_t ii = 0; ii < 7; ii++) {
+        Wire.beginTransmission(MS5637_ADDRESS);  // Initialize the Tx buffer
+        Wire.write(0xA0 | ii << 1);              // Put PROM address in Tx buffer
+        Wire.endTransmission(false);        // Send the Tx buffer, but send a restart to keep connection alive
+        uint8_t i = 0;
+        Wire.requestFrom(MS5637_ADDRESS, 2);   // Read two bytes from slave PROM address 
+        while (Wire.available()) {
+            data[i++] = Wire.read(); }               // Put read results in the Rx buffer
+        destination[ii] = (uint16_t) (((uint16_t) data[0] << 8) | data[1]); // construct PROM data for return to main program
+    }
+}
+
+static uint32_t MS5637Read(uint8_t CMD, uint8_t OSR)  // temperature data read
+{
+    uint8_t data[3] = {0,0,0};
+    Wire.beginTransmission(MS5637_ADDRESS);  // Initialize the Tx buffer
+    Wire.write(CMD | OSR);                  // Put pressure conversion command in Tx buffer
+    Wire.endTransmission(false);        // Send the Tx buffer, but send a restart to keep connection alive
+
+    switch (OSR)
+    {
+        case ADC_256: delay(1); break;  // delay for conversion to complete
+        case ADC_512: delay(3); break;
+        case ADC_1024: delay(4); break;
+        case ADC_2048: delay(6); break;
+        case ADC_4096: delay(10); break;
+        case ADC_8192: delay(20); break;
+    }
+
+    Wire.beginTransmission(MS5637_ADDRESS);  // Initialize the Tx buffer
+    Wire.write(0x00);                        // Put ADC read command in Tx buffer
+    Wire.endTransmission(false);        // Send the Tx buffer, but send a restart to keep connection alive
+    uint8_t i = 0;
+    Wire.requestFrom(MS5637_ADDRESS, 3);     // Read three bytes from slave PROM address 
+    while (Wire.available()) {
+        data[i++] = Wire.read(); }               // Put read results in the Rx buffer
+    return (uint32_t) (((uint32_t) data[0] << 16) | (uint32_t) data[1] << 8 | data[2]); // construct PROM data for return to main program
+}
+
+
+
+static unsigned char MS5637checkCRC(uint16_t * n_prom)  // calculate checksum from PROM register contents
+{
+    int cnt;
+    unsigned int n_rem = 0;
+    unsigned char n_bit;
+
+    n_prom[0] = ((n_prom[0]) & 0x0FFF);  // replace CRC byte by 0 for checksum calculation
+    n_prom[7] = 0;
+    for(cnt = 0; cnt < 16; cnt++)
+    {
+        if(cnt%2==1) n_rem ^= (unsigned short) ((n_prom[cnt>>1]) & 0x00FF);
+        else         n_rem ^= (unsigned short)  (n_prom[cnt>>1]>>8);
+        for(n_bit = 8; n_bit > 0; n_bit--)
+        {
+            if(n_rem & 0x8000)    n_rem = (n_rem<<1) ^ 0x3000;
+            else                  n_rem = (n_rem<<1);
+        }
+    }
+    n_rem = ((n_rem>>12) & 0x000F);
+    return (n_rem ^ 0x00);
+}
+
+
 void setup()
 {
     Wire.begin();
@@ -151,113 +228,4 @@ void loop()
 
 } // loop()
 
-// For the MS5637, we write commands, and the MS5637 sends data in response, rather than directly reading
-// MS5637 registers
 
-void MS5637Reset()
-{
-    Wire.beginTransmission(MS5637_ADDRESS);  // Initialize the Tx buffer
-    Wire.write(MS5637_RESET);                // Put reset command in Tx buffer
-    Wire.endTransmission();                  // Send the Tx buffer
-}
-
-void MS5637PromRead(uint16_t * destination)
-{
-    uint8_t data[2] = {0,0};
-    for (uint8_t ii = 0; ii < 7; ii++) {
-        Wire.beginTransmission(MS5637_ADDRESS);  // Initialize the Tx buffer
-        Wire.write(0xA0 | ii << 1);              // Put PROM address in Tx buffer
-        Wire.endTransmission(false);        // Send the Tx buffer, but send a restart to keep connection alive
-        uint8_t i = 0;
-        Wire.requestFrom(MS5637_ADDRESS, 2);   // Read two bytes from slave PROM address 
-        while (Wire.available()) {
-            data[i++] = Wire.read(); }               // Put read results in the Rx buffer
-        destination[ii] = (uint16_t) (((uint16_t) data[0] << 8) | data[1]); // construct PROM data for return to main program
-    }
-}
-
-uint32_t MS5637Read(uint8_t CMD, uint8_t OSR)  // temperature data read
-{
-    uint8_t data[3] = {0,0,0};
-    Wire.beginTransmission(MS5637_ADDRESS);  // Initialize the Tx buffer
-    Wire.write(CMD | OSR);                  // Put pressure conversion command in Tx buffer
-    Wire.endTransmission(false);        // Send the Tx buffer, but send a restart to keep connection alive
-
-    switch (OSR)
-    {
-        case ADC_256: delay(1); break;  // delay for conversion to complete
-        case ADC_512: delay(3); break;
-        case ADC_1024: delay(4); break;
-        case ADC_2048: delay(6); break;
-        case ADC_4096: delay(10); break;
-        case ADC_8192: delay(20); break;
-    }
-
-    Wire.beginTransmission(MS5637_ADDRESS);  // Initialize the Tx buffer
-    Wire.write(0x00);                        // Put ADC read command in Tx buffer
-    Wire.endTransmission(false);        // Send the Tx buffer, but send a restart to keep connection alive
-    uint8_t i = 0;
-    Wire.requestFrom(MS5637_ADDRESS, 3);     // Read three bytes from slave PROM address 
-    while (Wire.available()) {
-        data[i++] = Wire.read(); }               // Put read results in the Rx buffer
-    return (uint32_t) (((uint32_t) data[0] << 16) | (uint32_t) data[1] << 8 | data[2]); // construct PROM data for return to main program
-}
-
-
-
-unsigned char MS5637checkCRC(uint16_t * n_prom)  // calculate checksum from PROM register contents
-{
-    int cnt;
-    unsigned int n_rem = 0;
-    unsigned char n_bit;
-
-    n_prom[0] = ((n_prom[0]) & 0x0FFF);  // replace CRC byte by 0 for checksum calculation
-    n_prom[7] = 0;
-    for(cnt = 0; cnt < 16; cnt++)
-    {
-        if(cnt%2==1) n_rem ^= (unsigned short) ((n_prom[cnt>>1]) & 0x00FF);
-        else         n_rem ^= (unsigned short)  (n_prom[cnt>>1]>>8);
-        for(n_bit = 8; n_bit > 0; n_bit--)
-        {
-            if(n_rem & 0x8000)    n_rem = (n_rem<<1) ^ 0x3000;
-            else                  n_rem = (n_rem<<1);
-        }
-    }
-    n_rem = ((n_rem>>12) & 0x000F);
-    return (n_rem ^ 0x00);
-}
-
-
-void writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
-{
-    Wire.beginTransmission(address);  // Initialize the Tx buffer
-    Wire.write(subAddress);           // Put slave register address in Tx buffer
-    Wire.write(data);                 // Put data in Tx buffer
-    Wire.endTransmission();           // Send the Tx buffer
-}
-
-uint8_t readByte(uint8_t address, uint8_t subAddress)
-{
-    uint8_t data; // `data` will store the register data	 
-    Wire.beginTransmission(address);         // Initialize the Tx buffer
-    Wire.write(subAddress);	                 // Put slave register address in Tx buffer
-    Wire.endTransmission(false);        // Send the Tx buffer, but send a restart to keep connection alive
-    //	Wire.endTransmission(false);             // Send the Tx buffer, but send a restart to keep connection alive
-    //	Wire.requestFrom(address, 1);  // Read one byte from slave register address 
-    Wire.requestFrom(address, (size_t) 1);   // Read one byte from slave register address 
-    data = Wire.read();                      // Fill Rx buffer with result
-    return data;                             // Return data read from slave register
-}
-
-void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest)
-{  
-    Wire.beginTransmission(address);   // Initialize the Tx buffer
-    Wire.write(subAddress);            // Put slave register address in Tx buffer
-    Wire.endTransmission(false);  // Send the Tx buffer, but send a restart to keep connection alive
-    //	Wire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
-    uint8_t i = 0;
-    //        Wire.requestFrom(address, count);  // Read bytes from slave register address 
-    Wire.requestFrom(address, (size_t) count);  // Read bytes from slave register address 
-    while (Wire.available()) {
-        dest[i++] = Wire.read(); }         // Put read results in the Rx buffer
-}
